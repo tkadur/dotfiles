6e456f68dcce55730dc66ce166397f7c22ad22b7
tkadur <thejas.kadur@gmail.com>
2017-08-30 20:14:34 -0400

    shell: Added antigen for Andrew

--- host-andrew/zsh/host.zsh
+++ host-andrew/zsh/host.zsh
@@ -1,5 +1,5 @@
-# export ANTIGEN_DIR=/usr/local/share/antigen
+export ANTIGEN_DIR=$USER_HOME/.antigen
 
 # test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
 
-# source ~/.zsh/antigen-config.zsh
+source ~/.zsh/antigen-config.zsh
