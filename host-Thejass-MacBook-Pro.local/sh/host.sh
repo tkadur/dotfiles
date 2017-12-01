f1d8185b5ae3d440b939721497a104f9c507c100
tkadur <thejas.kadur@gmail.com>
2017-11-30 20:41:30 -0500

    shell: Added clang 5.0 stuff for Mac

--- host-Thejass-MacBook-Pro.local/sh/host.sh
+++ host-Thejass-MacBook-Pro.local/sh/host.sh
@@ -1,2 +1,6 @@
 export USER_HOME=/Users/Thejas
 export RUST_SRC_PATH=$USER_HOME/.multirust/toolchains/nightly-x86_64-apple-darwin/lib/rustlib/src/rust/src/
+export CLANG_PATH=/usr/local/Cellar/llvm/5.0.0/bin
+export CC=/usr/local/Cellar/llvm/5.0.0/bin/clang
+export CXX=/usr/local/Cellar/llvm/5.0.0/bin/clang++
+export PATH="/usr/local/opt/llvm/bin:$PATH"
