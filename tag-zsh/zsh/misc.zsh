e056a41b1693cc9c1bee090848c97109462abf04
Thejaswi Kadur <thejas.kadur@gmail.com>
2018-08-23 22:35:58 -0400

    zsh: Completely revamped setup
    The old setup was too complex and terribly fragile.
    This new setup uses zplug along with just a handful of plugins for a
    minimal setup with all the features I realistically need.

--- tag-zsh/zsh/misc.zsh
+++ tag-zsh/zsh/misc.zsh
@@ -1,6 +1,23 @@
 # Add fuzzy matching to completion
+# 0 -- vanilla completion (abc => abc)
+# 1 -- smart case completion (abc => Abc)
+# 2 -- word flex completion (abc => A-big-Car)
+# 3 -- full flex completion (abc => ABraCadabra)
 # From http://superuser.com/a/815317
 zstyle ':completion:*' matcher-list '' \
 	'm:{a-z\-}={A-Z\_}' \
 	'r:[^[:alpha:]]||[[:alpha:]]=** r:|=* m:{a-z\-}={A-Z\_}' \
 	'r:|?=** m:{a-z\-}={A-Z\_}'
+
+##############################################################################
+# History Configuration
+##############################################################################
+HISTSIZE=5000               #How many lines of history to keep in memory
+HISTFILE=$HOME/.zsh_history     #Where to save history to disk
+SAVEHIST=50000               #Number of history entries to save to disk
+setopt hist_expire_dups_first # delete duplicates first when HISTFILE size exceeds HISTSIZE
+setopt hist_ignore_dups       # ignore duplicated commands history list
+setopt extended_history       # record timestamp of command in HISTFILE
+setopt appendhistory     #Append history to the history file (no overwriting)
+setopt sharehistory      #Share history across terminals
+setopt incappendhistory  #Immediately append to the history file, not just when a term is killed
