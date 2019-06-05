22c632307dafcc67c1234056d17c11f38e44cc52
Thejaswi Kadur <thejas.kadur@gmail.com>
2019-06-05 15:33:26 -0700

    sh: Removed unused lesspipe variable stuff

--- tag-sh/sh/variables.sh
+++ tag-sh/sh/variables.sh
@@ -13,10 +13,4 @@ add_to_path_silent "$HOME/.cargo/bin" "$HOME/.local/bin"
 # export LESS='-F -g -i -M -R -S -w -X -z-4'
 export LESS='-g -i -M -R -S -w -z-4'
 
-# Set the Less input preprocessor.
-# Try both `lesspipe` and `lesspipe.sh` as either might exist on a system.
-if (( $#commands[(i)lesspipe(|.sh)] )); then
-  export LESSOPEN="| /usr/bin/env $commands[(i)lesspipe(|.sh)] %s 2>&-"
-fi
-
 export PYTHONSTARTUP=~/.pythonrc
