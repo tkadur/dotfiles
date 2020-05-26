f81bc7988587f665ba2447061c230f31ce53e68d
Thejaswi Kadur <thejas.kadur@gmail.com>
2020-05-26 15:15:08 -0400

    host-Thejaswis-MacBook-Pro.local: Add iTerm2 config

--- host-Thejaswis-MacBook-Pro.local/sh/host/variables.sh
+++ host-Thejaswis-MacBook-Pro.local/sh/host/variables.sh
@@ -1,7 +1,7 @@
-if exists_command 'brew'; then
-  PREFIX=$(brew --prefix llvm)
-  add_to_path_priority "$PREFIX/bin"
-  LDFLAGS="$LDFLAGS,-L$PREFIX/lib -Wl,-rpath,$PREFIX/lib"
-fi
+# if exists_command 'brew'; then
+#   PREFIX=$(brew --prefix llvm)
+#   add_to_path_priority "$PREFIX/bin"
+#   LDFLAGS="$LDFLAGS,-L$PREFIX/lib -Wl,-rpath,$PREFIX/lib"
+# fi
 
 add_to_path_silent "/usr/local/texlive/2019/bin/x86_64-darwin"
