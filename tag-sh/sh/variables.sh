811e9605ff9e93538871d33bf36a33f22c6f6090
Thejaswi Kadur <thejas.kadur@gmail.com>
2020-05-28 18:47:00 -0400

    sh: Remove commented code

--- tag-sh/sh/variables.sh
+++ tag-sh/sh/variables.sh
@@ -9,10 +9,6 @@ export PAGER='less'
 
 add_to_path_silent "$HOME/.cargo/bin" "$HOME/.local/bin" "/usr/local/smlnj/bin"
 
-# Set the default Less options.
-# Mouse-wheel scrolling has been disabled by -X (disable screen clearing).
-# Remove -X and -F (exit if the content fits on one screen) to enable it.
-# export LESS='-F -g -i -M -R -S -w -X -z-4'
 export LESS='-g -i -M -R -S -w -z-4'
 
 export PYTHONSTARTUP=~/.pythonrc
