70fcdaa93cf5fa767f1951e216cacab06f87fad1
Thejaswi Kadur <thejas.kadur@gmail.com>
2022-05-11 16:14:44 -0700

    sh: Update env vars for GBA development

--- host-Thejaswis-MacBook-Pro.local/sh/host/variables.sh
+++ host-Thejaswis-MacBook-Pro.local/sh/host/variables.sh
@@ -5,3 +5,10 @@
 # fi
 
 add_to_path_silent "/usr/local/texlive/2019/bin/x86_64-darwin"
+
+export DEVKITPRO=/opt/devkitpro
+export DEVKITARM=$DEVKITPRO/devkitARM
+
+add_to_path_priority "$(brew --prefix)/opt/llvm/bin"
+export LDFLAGS="-L$(brew --prefix)/opt/llvm/lib -Wl,-rpath,$(brew --prefix)/opt/llvm/lib"
+export CPPFLAGS="-I/usr/local/opt/llvm/include"
